c1b710b40e452f19f697996c82111b10bb0be29b
YangL2 <y13526642167@163.com>
2023-05-26 18:40:06 +0800

    测试新包上传git

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * @author 理智
+ * @ Classname Main
+ * @ Description TODO
+ * @ Version 1.0.0
+ * @ Date 2023/5/26 18:37
+ * @ Created by y1352
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("啊打算几点");
+    }
+}
